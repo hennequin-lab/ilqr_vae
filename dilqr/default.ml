efcae03e31e03656d0339b8a800b3031417fda5a
hennequin <gje.hennequin@gmail.com>
2025-12-19 16:37:40 +0000

    user can control verbosity level in dilqr - useful for mpi logs

--- dilqr/default.ml
+++ dilqr/default.ml
@@ -1,5 +1,11 @@
-module Make (A : Prms.Intf.A) = struct
-  module Lqr = Lqr.Make (A)
+module Make (X : sig
+    module A : Prms.Intf.A
+
+    val verbose : bool
+  end) =
+struct
+  open X
+  module Lqr = Lqr.Make (X)
   module Bmo = Bmo.Make (A)
   module AD = Bmo.AD
   open AD.Builder
